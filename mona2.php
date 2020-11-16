60dd3fcac0c03516b6bfd584959aedc5707892f5
monaabdalmoniem <74499359+monaabdalmoniem@users.noreply.github.com>
2020-11-16 10:08:36 -0800

    Add files via upload

---
+++ mona2.php
@@ -0,0 +1,35 @@
+<! DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>mona</title>
+</head>
+<body bgcolor="green">
+
+<center>
+<h1>بسم الله الرحمن الرحيم</h1>
+<br>
+<h3>كلية الامارات للعلوم والتكنولوجيا </h3>
+<?php
+$name="Mona AbdALmonam";
+$class="IT 3";
+$subject="PHP";
+$addess="khartoum";
+$ID_student="31013947";
+echo"$name"."<br>";
+echo"<br>";
+echo"class:"."$class"."<br>";
+echo"<br>";
+echo"subject:"."$subject"."<br>";
+echo"<br>";
+echo"addess:"."$addess"."<br>";
+echo"<br>";
+echo"ID_student:"."$ID_student"."<br>";
+echo"<br>";
+
+?>
+
+</center>
+</body>
+</html>
